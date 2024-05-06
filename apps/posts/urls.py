f16486907ea3620b7ce6c49b372365031c071fb4
BailAngels <wolf.abmin.14@gmail.com>
2024-05-06 18:16:57 +0600

    second_project

---
+++ apps/posts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from apps.posts import views
+
+
+urlpatterns = [
+#     path('', views.index, name='homepage'),
+#     path('create/', views.create, name='create'),
+#     path('detail/<int:pk>/', views.retrieve, name='detail'),
+#     path('update/<int:pk>/', views.update, name='update'),
+#     path('delete/<int:pk>/', views.destroy, name='destroy'),
+]
---
+++ apps/posts/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render, redirect
+
+from apps.posts.models import Post
+from apps.tags.models import Tag
+
+
